ea744a075d2280231b6af5cb89a0a6567c8e1393
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-08-15 14:41:52 -0400

    -Removed bootstrap.sass and including bootstrap.css

--- dapp/frontend/app/scripts/controllers/signup.js
+++ dapp/frontend/app/scripts/controllers/signup.js
@@ -9,9 +9,8 @@
  */
 angular.module('EscrowRajApp')
   .controller('SignupCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+      $scope.seed = '';
+      $scope.generateWallet = function () {
+          $scope.seed = 'hello world';
+      };
   });
--- dapp/frontend/test/karma.conf.js
+++ dapp/frontend/test/karma.conf.js
@@ -24,7 +24,6 @@ module.exports = function(config) {
       // bower:js
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
@@ -33,6 +32,7 @@ module.exports = function(config) {
       'bower_components/web3/dist/web3.js',
       'bower_components/web3/dist/web3.min.js',
       'bower_components/blockapps-js/api.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
