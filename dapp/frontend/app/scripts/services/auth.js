41ef1f69afcaa49301b32a8ba210d995b8b5996f
josh kaufman <skaufman@inrhythm.com>
2015-08-15 15:09:02 -0400

    adding register call to blocapp api

--- dapp/frontend/app/scripts/services/auth.js
+++ dapp/frontend/app/scripts/services/auth.js
@@ -8,18 +8,29 @@
  * Service in the EscrowRajApp.
  */
 angular.module('EscrowRajApp')
-  .service('auth', ['$location', function ($location) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('auth', ['$http', function ($http) {
+    var apiEndpoint = window.apiURL;
+    var ethlightjs = window.ethlightjs;
+
     this.user = null;
 
     this.isAuthenticated = function(){
       return !!this.user;
     };
 
-    this.ensureAuthenticated = function(){
-      if(!this.user) {
-        $location.path( "/login" );
-      }
+    this.authenticate = function (address){
+      return $http.get(apiEndpoint + '/query/account?address=' + userObj.address)
     };
+
+    this.register = function(user, keyStore) {
+      return $http.post(apiEndpoint + '/eth/v1.0/wallet', {
+        email: user.email,
+        loginpass: user.password,
+        address: keyStore.addresses[0],
+        enckey: keystore.serialize()
+      });
+    }
+
+    this.loadAccountInfo = function() {};
 
   }]);
