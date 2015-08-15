96e0b8959497a3c6d892377433d12660bd04118e
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-08-15 15:52:13 -0400

    -Working signup process

--- dapp/frontend/app/scripts/controllers/signup.js
+++ dapp/frontend/app/scripts/controllers/signup.js
@@ -10,26 +10,59 @@
 angular.module('EscrowRajApp')
 .controller('SignupCtrl', ['$scope', 'auth', function ($scope, auth) {
     var keystore;
-    $scope.email = '';
+    $scope.email = {
+        value: '',
+        valid: null
+    };
     $scope.seed = '';
-    $scope.password = '';
-    $scope.address = '';
+    $scope.encPassword = {
+        value: '',
+        valid: null
+    };
+    $scope.loginPassword = {
+        value: '',
+        valid: null
+    };
+    $scope.address = '<empty>';
+
+    var checkForNullValues = function (model) {
+        if (model.value.trim() === '') {
+            model.valid = false;
+        } else {
+            model.valid = true;
+        }
+        return model.valid;
+    };
 
     $scope.generateWallet = function () {
+        var user = {
+            email: '',
+            loginpass: ''
+        };
         if ($scope.seed === '' && $scope.randomizeSeedOption) {
             $scope.seed = ethlightjs.keystore.generateRandomSeed();
         }
-        if ($scope.password.trim() === '') {
-            $scope.emptyPassword = true;
+
+        if(!checkForNullValues($scope.email)) {
             return;
-        } else {
-            $scope.emptyPassword = false;
         }
 
-        keystore = new ethlightjs.keystore($scope.seed, $scope.password);
-        $scope.address = keystore.generateNewAddress($scope.password);
+        if(!checkForNullValues($scope.loginPassword)) {
+            return;
+        }
 
-        auth.register(keystore);
+        if(!checkForNullValues($scope.encPassword)) {
+            return;
+        }
+
+        keystore = new ethlightjs.keystore($scope.seed, $scope.encPassword.value);
+        $scope.address = keystore.generateNewAddress($scope.encPassword.value);
+
+        user.email = $scope.email.value;
+        user.loginpass = $scope.loginPassword.value;
+        user.address = $scope.address;
+
+        auth.register(user, keystore);
     };
 
     $scope.randomizeSeed = function () {
