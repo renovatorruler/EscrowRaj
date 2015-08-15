9d82e03157422d1c9c46d08381c75a7e67d68f70
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-08-15 19:46:59 -0400

    -Prefilling some values for easier interaction with the contract

--- dapp/frontend/app/scripts/controllers/buyer.js
+++ dapp/frontend/app/scripts/controllers/buyer.js
@@ -10,6 +10,9 @@
 angular.module('EscrowRajApp')
   .controller('BuyerCtrl', ['$scope', function ($scope) {
       $scope.encKeySecret = '';
+      $scope.sellerAddress = '46fa4c2d60305df40a74b8cbc04773d9bd5ad295';
+      $scope.etherAmount = '0.5';
+      $scope.memo = 'For Bar Mitzvah';
       $scope.sendContract = function () {
           escrow.createContract($scope.encKeySecret);
       };
