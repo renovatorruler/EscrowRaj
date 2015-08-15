0d2f9304a4ce702551c9894429a445601bb868fd
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-08-15 19:55:08 -0400

    fixes to buyer

--- dapp/frontend/app/scripts/controllers/buyer.js
+++ dapp/frontend/app/scripts/controllers/buyer.js
@@ -8,12 +8,13 @@
  * Controller of the EscrowRajApp
  */
 angular.module('EscrowRajApp')
-  .controller('BuyerCtrl', ['$scope', function ($scope) {
+  .controller('BuyerCtrl', ['$scope', 'escrow', function ($scope, escrow) {
       $scope.encKeySecret = '';
       $scope.sellerAddress = '46fa4c2d60305df40a74b8cbc04773d9bd5ad295';
       $scope.etherAmount = '0.5';
       $scope.memo = 'For Bar Mitzvah';
+
       $scope.sendContract = function () {
-          escrow.createContract($scope.encKeySecret);
+          console.debug(escrow.createContract($scope.encKeySecret));
       };
   }]);
