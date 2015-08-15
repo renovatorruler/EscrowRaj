e9c36ddf7db1d894da84404fa9842a958d3e5115
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-08-15 11:39:31 -0400

    -Created auth service

---
+++ dapp/frontend/app/scripts/services/auth.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name escrowRajApp.auth
+ * @description
+ * # auth
+ * Service in the escrowRajApp.
+ */
+angular.module('escrowRajApp')
+  .service('auth', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
